cd3c0495317f0a30ca4f17e2ceeaf1b4e06668be
datph <datphung239@gmail.com>
2023-04-09 15:10:28 +0700

    Add button affect when click, clean code HTML & JS

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const btns = document.querySelector("#btn-grid");
 let input = "";
-const operation = new Set(["x", "+", "-", "÷"]);
+const operation = new Set(["×", "+", "-", "÷"]);
 
 // Input
 let inputDisplay = document.querySelector("#display1 .result");
@@ -10,14 +10,22 @@ btns.addEventListener("click", (btn) => {
   // Input value
   let val = btn.target.innerText;
   let classOfBtn = btn.target.className;
-  // If input is number
+  // Number
   if (classOfBtn.includes("num")) {
     operation.has(input.slice(-1)) ? (input += " ") : true; // Add space after operation
     input += `${val}`;
-    // If input is operation
+    // Operator
   } else if (classOfBtn.includes("ops") && input.length !== 0) {
-    !operation.has(input.slice(-1)) ? (input += ` ${val}`) : true; // One operation only
-    // If input is a dot
+    // If dot at the end of number --> can not add operator
+    if (input.slice(-1) === ".") return;
+    // Input a operation,
+    !operation.has(input.slice(-1))
+      ? (input += ` ${val}`) // Add space after number
+      : // If other operator clicked then replace it with the last one
+      input.slice(-1) !== btn.target.innerText
+      ? (input = input.slice(0, -1) + btn.target.innerText)
+      : true;
+    // A dot
   } else if (classOfBtn.includes("dot") && input.length !== 0) {
     let last = input.split(" ").pop();
     if (!last.includes(".") && !isNaN(last.slice(-1))) input += val;
@@ -26,15 +34,17 @@ btns.addEventListener("click", (btn) => {
 });
 
 // Output
+let previousInput = document.querySelector("#sub-display > div.previous-input");
 const equal = document.querySelector("#equal");
 const outputDisplay = document
   .querySelector("#display")
   .querySelector(".result");
+// Function to calculate add subtract multiply and divide
 function calInArray(inputArr, operation) {
   let idx = inputArr.indexOf(operation);
   let before = parseFloat(inputArr[idx - 1]);
   let after = parseFloat(inputArr[idx + 1]);
-  operation === "x"
+  operation === "×"
     ? (result = before * after)
     : operation === "÷"
     ? (result = before / after)
@@ -46,14 +56,18 @@ function calInArray(inputArr, operation) {
   inputArr[idx - 1] = result;
   inputArr.splice(idx, 2);
 }
+// Execute when click on equal button
 equal.addEventListener("click", (_) => {
+  // If . at the end  --> return
+  if (input.slice(-1) === ".") return;
+  // Main
   const inputArr = input.split(" ");
   if (operation.has(inputArr[inputArr.length - 1])) return;
   // Mutiply and divided first
-  while (inputArr.indexOf("x") !== -1 || inputArr.indexOf("÷") !== -1) {
+  while (inputArr.indexOf("×") !== -1 || inputArr.indexOf("÷") !== -1) {
     for (let index = 0; index < inputArr.length; index++) {
-      if (inputArr[index] === "x") {
-        calInArray(inputArr, "x");
+      if (inputArr[index] === "×") {
+        calInArray(inputArr, "×");
         break;
       } else if (inputArr[index] === "÷") {
         calInArray(inputArr, "÷");
@@ -73,19 +87,22 @@ equal.addEventListener("click", (_) => {
     }
   }
   outputDisplay.innerText = inputArr[0];
+  previousInput.innerText = input;
+  input = "";
 });
-// Clear & Del
 
+// Clear & Del
 btns.addEventListener("click", (btn) => {
   // Clear
   if (btn.target.getAttribute("id") === "clear") {
     input = "";
     inputDisplay.innerText = "";
+    previousInput.innerText = "";
     outputDisplay.innerText = "";
   }
   // Del 1 charater
   if (btn.target.getAttribute("id") === "backspace") {
-    input = input.trim().slice(0, -1);
+    input = input.trim().slice(0, -1).trim();
     inputDisplay.innerText = input;
   }
 });
